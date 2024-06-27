dbb7375b96a9f74d1e8e2aeab7e15412093fd15c
quachnguyenbaothy <167504389+quachnguyenbaothy@users.noreply.github.com>
2024-06-28 06:02:27 +0700

    Update intro.js

--- constants/intro.js
+++ constants/intro.js
@@ -1,1 +1,102 @@
-
+const intros = [
+    {
+      id: "1",
+      name: "Rẫy khóm",
+      image:require("../assets/images/1.jpg"),
+      
+    },
+    {
+      id: "2",
+      name: "Khóm Cầu Đúc",
+      image: require("../assets/images/2.jpg"),
+     
+    },
+  
+    {
+      id: "3",
+      name: "Chợ nổi Ngã Bảy",
+      image: require("../assets/images/3.jpg"),
+     
+    },
+    {
+      id: "4",
+      name: "Chợ nổi Ngã Bảy",
+      image: require("../assets/images/4.jpg"),
+     
+    },
+    {
+      id: "5",
+      name: "Lung Ngọc Hoàng",
+      image: require("../assets/images/5.jpg"),
+      
+    },
+    {
+      id: "6",
+      name: "Lung Ngọc Hoàng",
+      image: require("../assets/images/6.jpg"),
+     
+    },
+    {
+        id: "7",
+        name: "Vườn Tràm Vị Thủy",
+        image: require("../assets/images/7.jpg"),
+       
+      },
+      {
+        id: "8",
+        name: "Vườn Tràm Vị Thủy",
+        image: require("../assets/images/8.jpg"),
+       
+      },
+      {
+        id: "9",
+        name: "Quýt đường Long Trị",
+        image: require("../assets/images/9.jpg"),
+       
+      },
+      {
+        id: "10",
+        name: "Quýt đường Long Trị",
+        image: require("../assets/images/10.jpg"),
+       
+      },
+      {
+        id: "11",
+        name: "Bưởi Năm Roi",
+        image: require("../assets/images/11.jpg"),
+       
+      },
+      {
+        id: "12",
+        name: "Bưởi Năm Roi",
+        image: require("../assets/images/12.jpg"),
+       
+      },
+      {
+        id: "13",
+        name: "Cá thát lát cườm",
+        image: require("../assets/images/13.jpg"),
+       
+      },
+      {
+        id: "14",
+        name: "Cá thát lát rút xương",
+        image: require("../assets/images/14.jpg"),
+       
+      },
+      {
+        id: "15",
+        name: "Rau chọi luộc",
+        image: require("../assets/images/15.jpg"),
+       
+      },
+      {
+        id: "16",
+        name: "Rau chọi",
+        image: require("../assets/images/16.jpg"),
+       
+      },
+  ];
+  
+  export default intros;
+  
